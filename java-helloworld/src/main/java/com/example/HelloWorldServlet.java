caa1381988d1c7f029cb843bbbfd98e7acda00da
vicky <63238951+vigneshnambiar@users.noreply.github.com>
2024-06-25 21:47:25 +0530

    Add files via upload

---
+++ java-helloworld/src/main/java/com/example/HelloWorldServlet.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().write("<h1>Hello, World!</h1>");
+    }
+}
+
